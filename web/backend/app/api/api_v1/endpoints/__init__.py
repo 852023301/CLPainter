78df4e843f88b07681ab3d2f2f7281e578263739
wjl <wjl@techfin.ai>
2026-01-29 22:53:43 +0800

    支持本都读取测试数据集

--- web/backend/app/api/api_v1/endpoints/__init__.py
+++ web/backend/app/api/api_v1/endpoints/__init__.py
@@ -1,10 +1,16 @@
 import requests
+import pickle
+from pathlib import Path
+import os
+
 
 def get_data_init():
-    response = requests.get(
-        url="https://echarts.apache.org/examples/data/asset/data/stock-DJI.json"
-    )
-    json_response = response.json()
+    # response = requests.get(
+    #     url="https://echarts.apache.org/examples/data/asset/data/stock-DJI.json"
+    # )
+    # json_response = response.json()
+    with open(Path(os.environ['appDir']) / "data_set/data_set.pkl", "rb", ) as f:
+        json_response = pickle.load(f)
     # 解析数据
     return json_response
 
