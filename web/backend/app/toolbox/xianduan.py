54f67a0282a4d0ac519dbf8decbf6baf4be0b3dc
wjl <wjl@techfin.ai>
2026-07-29 15:44:01 +0800

    feat(tools): 使用特征序列来优化fake last xianduan

--- web/backend/app/toolbox/xianduan.py
+++ web/backend/app/toolbox/xianduan.py
@@ -429,8 +429,7 @@ def generate_xian_duan(tzxl_list: List[TeZhengXuLie], bi_list: List[Union[Bi, Fa
         xianduan_finish_deque.append(xd_lm)
         xd_lm = xd_mr
 
-
-    # 制造fake last线段
+    ########################## 制造fake last线段
 
     last_xd: XianDuan = xianduan_finish_deque.pop()
     xianduan_finish_deque.append(last_xd)
@@ -441,24 +440,23 @@ def generate_xian_duan(tzxl_list: List[TeZhengXuLie], bi_list: List[Union[Bi, Fa
         fake_last_xd_direction_type = XianDuanDirectionType.UP
 
     fisrt_bi_index = last_xd.end_bi_idx
-    sl = slice(fisrt_bi_index, len(bi_list))
+    end_bi_index = last_xd.right_tzxl.right_bi_idx
+    sl = slice(end_bi_index, len(bi_list))
 
     bi_high_prices = np.array([bi.high_price for bi in bi_list[sl]])
     bi_low_prices = np.array([bi.low_price for bi in bi_list[sl]])
     local_max_idx = int(np.argmax(bi_high_prices))
     local_min_idx = int(np.argmin(bi_low_prices))
 
-    if fake_last_xd_direction_type == XianDuanDirectionType.UP:
-        end_bi_index = fisrt_bi_index + local_max_idx
-    else:
-        end_bi_index = fisrt_bi_index + local_min_idx
-
-
+    if fake_last_xd_direction_type == XianDuanDirectionType.UP and local_max_idx > 0 and bi_high_prices[local_max_idx] > \
+        bi_list[end_bi_index].high_price:
+        end_bi_index = end_bi_index + local_max_idx
+    elif fake_last_xd_direction_type == XianDuanDirectionType.DOWN and local_min_idx > 0 and bi_low_prices[
+        local_min_idx] < bi_list[end_bi_index].low_price:
+        end_bi_index = end_bi_index + local_min_idx
 
     first_bi = bi_list[fisrt_bi_index]
     end_bi = bi_list[end_bi_index]
-
-
 
     fake_last_xd = FakeXianDuanLast()
     fake_last_xd.start_bi_idx = fisrt_bi_index
@@ -467,11 +465,10 @@ def generate_xian_duan(tzxl_list: List[TeZhengXuLie], bi_list: List[Union[Bi, Fa
     fake_last_xd.end_idx = end_bi.end_idx
     fake_last_xd.start_time = first_bi.start_time
     fake_last_xd.end_time = end_bi.end_time
-    fake_last_xd.start_price =  first_bi.start_price
-    fake_last_xd.end_price =  end_bi.end_price
+    fake_last_xd.start_price = first_bi.start_price
+    fake_last_xd.end_price = end_bi.end_price
 
     fake_last_xd.xianduan_type = fake_last_xd_direction_type
-
 
     xianduan_finish_deque.append(fake_last_xd)
     xianduan_list = list(xianduan_finish_deque)
