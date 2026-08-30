d71e2c31b4da6a3195340171c655d16ae8723da7
wjl <wjl@techfin.ai>
2026-08-30 20:22:05 +0800

    发布1.1.0

--- web/backend/app/toolbox/tezhengxulie.py
+++ web/backend/app/toolbox/tezhengxulie.py
@@ -24,7 +24,7 @@ class TeZhengXuLie:
     """笔的特征序列"""
     type: TeZhengXuLieType = field(default=TeZhengXuLieType.DING)
     category: TeZhengXuLieCategory = field(default=TeZhengXuLieCategory.First)  # 默认第一类
-    bi_list: List[BiBase] = field(default_factory=list)  # 内部三笔列表
+    bi_list: List[BiBase] = field(default_factory=list,repr=False)  # 内部三笔列表
     bi_idx_list: List[int] = field(default_factory=list)  # 特征序列组件三笔在原始笔列表中的索引
 
     # tzxl索引(generate_te_zheng_xu_lie 事后填充)
