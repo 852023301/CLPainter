6bc6ba2665b9e9cda8207fe2bf15f42ca0ce17ca
wjl <wjl@techfin.ai>
2026-09-16 20:53:52 +0800

    优化

--- web/backend/app/data_set/data_from_tdata.py
+++ web/backend/app/data_set/data_from_tdata.py
@@ -8,7 +8,8 @@ import os
 from ddp import MysqlConnection
 from CLPainter.web.backend.app._config.settings import settings
 
-
+START_DATE = "2009-01-01"
+END_DATE = None
 def change_data_type_and_save(columns, dates, N_D, info_dict,
                               dsc_path,
                               precision=2):
@@ -55,51 +56,13 @@ if __name__ == "__main__":
     stock_info_dict = dict(
         zip(stock_info['stock_code'], pd.DatetimeIndex(stock_info['start_date']).strftime("%Y-%m-%d")))
 
-    d = fetch_data(["open", "close", "low", "high", "volume"])
+    d = fetch_data(["open", "close", "low", "high", "volume"], start_date=START_DATE, end_date=END_DATE)
     dates = pd.DatetimeIndex(d['open'].index).strftime("%Y-%m-%d").tolist()
     columns = d['open'].columns.tolist()
     stock_N_D = np.stack([d[i].values for i in d])
 
     change_data_type_and_save(columns, dates, stock_N_D, stock_info_dict,
                               dsc_path=Path(settings.DATA_DIR, "all_stocks"))
-    # for idx, i in enumerate(columns):
-    #     file_name = columns[idx].replace(".", "")
-    #     final_list = []
-    #     start_date = stock_info_dict.get(columns[idx])
-    #     if start_date is None:
-    #         continue
-    #     for jdx, j in enumerate(dates):
-    #         if dates[jdx] < start_date:
-    #             continue
-    #         _single = stock_N_D[:, jdx, idx]
-    #
-    #         if any(np.isnan(_single[:-1])) or any(_single[:-1] == 0):
-    #             print(f"{i} {dates[jdx]}存在空值或0，不处理")
-    #             print(_single)
-    #
-    #         single = [dates[jdx], *[float(fl).__round__(2) for fl in _single]]
-    #         final_list.append(single)
-    #
-    #     with open(f"/home/wjl/TechFinWorkSpace/CLPainter/web/backend/app/data_set/all_stocks/{file_name}.pkl", "wb") as f:
-    #         pickle.dump(final_list, f)
-
-    # 容器内执行 rm -r  /root/CLPainter/web/backend/app/data_set/all_stocks
-    # 容器外执行 docker cp /home/wjl/TechFinWorkSpace/CLPainter/web/backend/app/data_set/all_stocks CLPainter:/root/CLPainter/web/backend/app/data_set/
-
-    #########################################
-    # # 旧版取指数
-    # index_name = "szzs"
-    # columns = ["open", "close", "low", "high", "volume"]
-    # data = fetch_data([f"{index_name}_{i}" for i in columns])
-    # concat_data = pd.concat(data, axis=1)
-    # concat_data.columns = columns
-    # concat_data.index = pd.DatetimeIndex(concat_data.index).strftime("%Y-%m-%d")
-    # final_list = concat_data.reset_index().values.tolist()
-    #
-    # with open(f"/home/wjl/TechFinWorkSpace/CLPainter/web/backend/app/data_set/data_set_000001SH.pkl", "wb") as f:
-    #     pickle.dump(final_list, f)
-
-    #  docker cp /home/wjl/TechFinWorkSpace/CLPainter/web/backend/app/data_set/data_set_000002SH.pkl CLPainter:/root/CLPainter/web/backend/app/data_set
 
     #########################################
     #  指数
@@ -133,7 +96,7 @@ if __name__ == "__main__":
     for index_name, code in index_map.items():
         start_date = index_info_dict.get(code)
         columns = ["open", "close", "low", "high", "volume"]
-        data = fetch_data([f"{index_name}_{i}" for i in columns])
+        data = fetch_data([f"{index_name}_{i}" for i in columns],start_date=START_DATE,end_date=END_DATE)
         concat_data = pd.concat(data, axis=1)
         concat_data.columns = columns
         concat_data = concat_data[concat_data.index >= start_date]
@@ -152,7 +115,6 @@ if __name__ == "__main__":
                 if file_names.get(f) is None:
                     os.remove(os.path.join(dsc_path, f))
 
-    #  docker cp /home/wjl/TechFinWorkSpace/CLPainter/web/backend/app/data_set/all_index CLPainter:/root/CLPainter/web/backend/app/data_set
 
     #########################################
     # etf
@@ -163,35 +125,10 @@ if __name__ == "__main__":
     etf_info_dict = dict(zip(etf_info['stock_code'], pd.DatetimeIndex(etf_info['start_date']).strftime("%Y-%m-%d")))
 
     cols = [f"etf_{i}" for i in ["open", "close", "low", "high", "volume"]]
-    d = fetch_data(cols)
+    d = fetch_data(cols,start_date=START_DATE,end_date=END_DATE)
     dates = pd.DatetimeIndex(d[cols[0]].index).strftime("%Y-%m-%d").tolist()
     columns = d[cols[0]].columns.tolist()
     etf_N_D = np.stack([d[i].values for i in d])
 
     change_data_type_and_save(columns, dates, etf_N_D, etf_info_dict,
                               dsc_path=Path(settings.DATA_DIR, "all_etf"), precision=3)
-    # for idx, i in enumerate(columns):
-    #     file_name = columns[idx].replace(".", "")
-    #     final_list = []
-    #     start_date = etf_info_dict.get(columns[idx])
-    #     if start_date is None:
-    #         continue
-    #
-    #     for jdx, j in enumerate(dates):
-    #         if dates[jdx] < start_date:
-    #             continue
-    #         _single = etf_N_D[:, jdx, idx]
-    #
-    #         if any(np.isnan(_single[:-1])) or any(_single[:-1] == 0):
-    #             print(f"{i} {dates[jdx]}存在空值或0，不处理")
-    #             print(_single)
-    #
-    #             break
-    #         single = [dates[jdx], *[float(fl).__round__(3) for fl in _single]]
-    #
-    #         final_list.append(single)
-    #
-    #     with open(f"/home/wjl/TechFinWorkSpace/CLPainter/web/backend/app/data_set/all_etf/{file_name}.pkl", "wb") as f:
-    #         pickle.dump(final_list, f)
-
-    # 容器外执行 docker cp /home/wjl/TechFinWorkSpace/CLPainter/web/backend/app/data_set/all_etf CLPainter:/root/CLPainter/web/backend/app/data_set/
